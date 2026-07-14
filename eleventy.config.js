cea7a0ec52a4f63dddc1c9ab45a56bcfc43e1b6d
Julian - Elekio <julian@elekio.fr>
2026-07-14 10:38:47 +0000

    Ajout _redirects, sitemap, cleanup

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,6 +6,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/style.css": "style.css" });
   eleventyConfig.addPassthroughCopy({ "src/images": "images" });
   eleventyConfig.addPassthroughCopy({ "src/robots.txt": "robots.txt" });
+  eleventyConfig.addPassthroughCopy({ "src/sitemap.xml": "sitemap.xml" });
+  eleventyConfig.addPassthroughCopy({ "src/_redirects": "_redirects" });
 
   // Copier les assets racine (logo, favicon, etc.)
   const rootAssets = [
@@ -14,7 +16,6 @@ module.exports = function (eleventyConfig) {
     "favicon.png",
     "apple-touch-icon.png",
     "robots.txt",
-    "sitemap.xml",
   ];
   rootAssets.forEach((file) => {
     const src = path.join(__dirname, file);
